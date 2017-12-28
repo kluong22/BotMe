42248026a5cc5873bf642e49feb5c506308c0e88
Kevin Luong <kluong@qualtrics.com>
2017-12-28 11:19:50 -0800

    Updated code quality

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 # BotMe
 Twitter Bot which tries to impersonate users
+
 Uses: https://github.com/tweepy and https://github.com/jsvine/markovify
 
+How to use:
+* Fill out the botmeconstants with your twitter credentials
+* run botme.py with the username as a command line argument
+* enjoy your new tweets
--- botme.py
+++ botme.py
@@ -1,47 +1,55 @@
 import markovify
 import tweepy
+import botmeconstants
+import sys
 
-username = 
-consumer_key = 
-consumer_secret = 
-access_token = 
-access_token_secret =  
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
-api = tweepy.API(auth)
-user = api.get_user(username)
-num_tweets = 5
-status_id = 0
-text_list = []
-def get_more_tweets(username, status_id):
-	x = api.user_timeline(id = username, count = 200, max_id = status_id)
-	for status in x:
-		text = unicode(status.text).encode('utf8')
+create_num_tweets = 5
+max_tweet_request = 200
+max_tweet_length = 140
+
+def tweet_request(username, status_id):
+	tweets = api.user_timeline(id = username, count = max_tweet_request,
+	 max_id = status_id)
+	for tweet in tweets:
+		text = unicode(tweet.text).encode('utf8')
 		text+='\n'
-		text_list.append(text)
+		.append(text)
 		status_id = status.id
 	return status_id
 
-x = api.user_timeline(id = username, count = 200)
-for status in x:
-	text = unicode(status.text).encode('utf8')
-	text_list.append(text)
-	status_id = status.id
-counter = 0
-while True:
-	new_id = get_more_tweets(username,status_id)
-	if (new_id == status_id):
-		break
-	else:
+def get_tweets(username):
+	status_id = 0
+	tweet_list = []
+	x = api.user_timeline(id = username, count = max_tweet_request)
+	for status in x:
+		text = unicode(status.text).encode('utf8')
+		tweet_list.append(text)
+		status_id = status.id
+
+	while (new_id != status_id)
+		new_id = tweet_request(username,status_id)
 		status_id = new_id
+	return tweet_list
 
+def create_tweets(tweet_list):
+	tweet_list = ''.join(tweet_list)
+	text_model = markovify.Text(tweet_list)
 
+	for i in range(num_tweets):
+		status_update = (text_model.make_short_sentence(max_tweet_length))
+		if not ('@' in status_update):
+			api.update_status(status = status_update)
 
+def main(argv):
+	# Authentication
+	username = argv[1]
+	auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+	auth.set_access_token(access_token, access_token_secret)
+	api = tweepy.API(auth)
+	user = api.get_user(username)
 
-text_list = ''.join(text_list)
-text_model = markovify.Text(text_list)
+	tweet_list = get_tweets(username)
+	create_tweets(tweet_list)
 
-for i in range(num_tweets):
-	status_update = (text_model.make_short_sentence(140))
-	if not ('@' in status_update):
-		api.update_status(status = status_update)
+if __name__ = "__main__":
+	main(sys.argv)
